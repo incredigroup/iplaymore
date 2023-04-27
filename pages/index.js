fda2cceee82c6f383b380409ce2705456fd2bb56
High <113913706+incredigroup@users.noreply.github.com>
2023-04-27 18:09:01 +0900

    Redeem 6 digit code
    Made page of Redeem 6 digit code

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import Novel from "../src/components/Novel";
 import Layout from "../src/layouts/Layout";
 import Bonfire from "../src/components/Bonfire";
 import Marketplace from "../src/components/Marketplace";
+import EmptyPage from "../src/components/EmptyPage";
 
 const YOUTUBE_PLAYLIST_ITEMS_API =
   "https://www.googleapis.com/youtube/v3/playlistItems";
@@ -23,6 +24,7 @@ const Index = ({ videosData, bonfireData , marketplaceData}) => {
       <Register />
       <Videos data={videosData} />
       <Marketplace data = {marketplaceData} />
+      <EmptyPage/>
       <Login />
     </Layout>
   );
--- src/components/cover/Seach_bar.js
+++ src/components/cover/Seach_bar.js
@@ -1,6 +1,13 @@
 import Image from 'next/image';
 import { useRef } from "react";
-const Seach_bar = ({page,  url }) => {
+import { useContext } from "react";
+import { context } from "../../context/context";
+
+const Seach_bar = ({route,  page,  url }) => {
+    const navContext = useContext(context);
+    const { nav, changeNav, logined, logStatus, username } = navContext;
+    const activeNav = (value) => (value == nav ? "active" : "");
+
     return (
         <div class="seach_nav">
         <div className=''>
@@ -15,7 +22,7 @@ const Seach_bar = ({page,  url }) => {
         <a href="#about">Brood Characters</a>
         <a className='end' href="#contact">Kin Chacaters</a>
         </div>
-        <a className='gra_red' href="#contact">Redeem Code</a>
+        <a className='gra_red' href="#emptypage" onClick={() => changeNav("emptypage")}>Redeem Code</a>
       </div>
     );
 };
